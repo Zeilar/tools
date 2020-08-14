81e7472ccf8ebf729fb7c363cd17f8d30c57d118
Philip <philip_angelin@hotmail.com>
2020-08-14 13:59:11 +0200

    Shadow generator

--- src/components/ShadowGenerator.js
+++ src/components/ShadowGenerator.js
@@ -1,67 +1,75 @@
-import React, { useState, useRef } from 'react';
 import { createUseStyles } from 'react-jss';
+import React, { useState } from 'react';
 import Input from './ShadowGeneratorInput';
 
 export default function ShadowGenerator() {
     const styles = createUseStyles({
         shadowGenerator: {
             'justify-content': 'center',
-            'flex-direction': 'column',
+            'flex-direction': 'row',
             'align-items': 'center',
+            'font-size': '1.35rem',
             display: 'flex',
         },
         shadowContainer: {
             'justify-content': 'center',
             'flex-direction': 'row',
             'align-items': 'center',
+            'margin-left': '3rem',
             background: 'white',
             display: 'flex',
-            height: '35vh',
-            width: '35vh',
+            height: '60vh',
+            width: '60vh',
         },
         shadowText: {
             color: 'black',
         },
         shadowInputs: {
             'flex-direction': 'column',
-            'margin-top': '2rem',
+            display: 'flex',
+            width: '60vh',
+        },
+        inputInsetWrapper: {
+            'justify-content': 'center',
+            'flex-direction': 'row',
+            'align-items': 'center',
             display: 'flex',
         },
-        inputSlider: {
-            border: '1px solid rgb(175, 175, 175)',
-            background: 'rgb(190, 190, 190)',
-            'border-radius': '1rem',
-            appearance: 'none',
-            height: '0.5rem',
-            '&::-webkit-slider-thumb': {
-                border: '1px solid rgb(190, 190, 190)',
-                'border-radius': '50%',
-                background: 'white',
-                appearance: 'none',
-                height: '1.5rem',
-                width: '1.5rem',
-            }
+        inputInset: {
+            'margin-left': '0.5rem',
+        },
+        inputInsetLabel: {
+            'user-select': 'none',
         },
     });
     const classes = styles();
 
-    const [color, setColor] = useState('rgba(0, 0, 0, 0.75)');
+    const [opacity, setOpacity] = useState(50);
     const [offsetX, setOffsetX] = useState(0);
     const [offsetY, setOffsetY] = useState(0);
-    const [spread, setSpread] = useState(0);
-    const [inset, setInset] = useState('');
-    const [blur, setBlur] = useState(5);
+    const [inset, setInset] = useState(false);
+    const [spread, setSpread] = useState(5);
+    const [blur, setBlur] = useState(25);
+
+    const boxShadow = `${offsetX}px ${offsetY}px ${blur}px ${spread}px ${inset ? 'inset' : ''} rgba(0, 0, 0, ${opacity})`;
 
     return (
         <div className={classes.shadowGenerator} id="content">
-            <div className={classes.shadowContainer} style={{ boxShadow: `${offsetX}px ${offsetY}px ${blur}px ${spread}px ${inset} ${color}` }}>
-                <p className={classes.shadowText}>{`box-shadow: ${offsetX}px ${offsetY}px ${blur}px ${spread}px ${inset} ${color};`}</p>
-            </div>
             <div className={classes.shadowInputs}>
-                <Input setter={setOffsetX} min="-25" max="25" default="0" />
-                <Input setter={setOffsetY} min="-25" max="25" default="0" />
-                <Input setter={setBlur} min="0" max="50" default="25" />
-                <Input setter={setSpread} min="0" max="10" default="5" />
+                <Input text="Offset X" setter={setOffsetX} min="-25" max="25" default="0" />
+                <Input text="Offset Y" setter={setOffsetY} min="-25" max="25" default="0" />
+                <Input text="Blur" setter={setBlur} min="0" max="50" default="25" />
+                <Input text="Spread" setter={setSpread} min="0" max="10" default="5" />
+                <Input text="Opacity" setter={setOpacity} min="0" max="100" default="50" />
+                <div className={classes.inputInsetWrapper}>
+                    <label className={classes.inputInsetLabel} htmlFor="test">Inset</label>
+                    <input className={classes.inputInset} type="checkbox" onChange={() => setInset(!inset)} id="test" />
+                </div>
+            </div>
+            <div className={classes.shadowContainer} style={{ boxShadow: boxShadow }}>
+                <p className={classes.shadowText}>
+                    {`box-shadow: ${boxShadow}`}
+                </p>
             </div>
         </div>
     );
--- src/components/ShadowGeneratorInput.js
+++ src/components/ShadowGeneratorInput.js
@@ -9,6 +9,8 @@ export default function ShadowGeneratorInput(props) {
             'border-radius': '1rem',
             appearance: 'none',
             height: '0.5rem',
+            margin: '0 1rem',
+            flex: '10',
             '&::-webkit-slider-thumb': {
                 border: '1px solid rgb(190, 190, 190)',
                 'border-radius': '50%',
@@ -16,7 +18,26 @@ export default function ShadowGeneratorInput(props) {
                 appearance: 'none',
                 height: '1.5rem',
                 width: '1.5rem',
-            }
+            },
+        },
+        inputsWrapper: {
+            'justify-content': 'space-between',
+            'flex-direction': 'row',
+            'align-items': 'center',
+            margin: '1rem 0',
+            display: 'flex',
+            height: '2rem',
+        },
+        resetButton: {
+            border: '1px solid rgb(175, 175, 175)',
+            'border-radius': '0.5rem',
+            background: 'red',
+            padding: '0.5rem',
+            flex: '1',
+            color: 'white',
+        },
+        inputText: {
+            flex: '2',
         },
     });
     const classes = styles();
@@ -28,12 +49,13 @@ export default function ShadowGeneratorInput(props) {
     }
 
     return (
-        <div className={classes.shadowInputsWrapper}>
+        <div className={classes.inputsWrapper}>
+            <span className={classes.inputText}>{props.text}</span>
             <input 
                 className={classes.inputSlider} onChange={(e) => props.setter(e.target.value)} ref={input}
                 type="range" min={props.min} max={props.max} defaultValue={props.default}
             />
-            <button onClick={reset}>Reset</button>
+            <button className={classes.resetButton} onClick={reset}>Reset</button>
         </div>
     );
 }
